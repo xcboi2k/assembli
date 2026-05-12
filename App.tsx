a915ef04c603c4513aef16276e6a545483c497f7
Jose Alexei Garcia <darkspikyshark@gmail.com>
2026-05-12 09:20:12 +0800

    feat: Initialize Sentry

--- App.tsx
+++ App.tsx
@@ -7,7 +7,6 @@ import {
     SpaceGrotesk_600SemiBold,
     SpaceGrotesk_700Bold,
 } from '@expo-google-fonts/space-grotesk'
-
 import {
     Inter_400Regular,
     Inter_500Medium,
@@ -15,12 +14,20 @@ import {
 } from '@expo-google-fonts/inter'
 import { NavigationContainer } from '@react-navigation/native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import * as Sentry from '@sentry/react-native'
 
 import { RootNavigator } from '@/navigation'
 
 SplashScreen.preventAutoHideAsync()
 
-export default function App() {
+Sentry.init({
+    dsn: 'https://1973f8127c62cdd92a976f6ec20f9d91@o4511221589475328.ingest.us.sentry.io/4511374275051520',
+    // Adds more context data to events (IP address, cookies, user, etc.)
+    // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+    sendDefaultPii: true,
+})
+
+export default Sentry.wrap(function App() {
     const [loaded] = useFonts({
         SpaceGrotesk_400Regular,
         SpaceGrotesk_600SemiBold,
@@ -45,4 +52,4 @@ export default function App() {
             </NavigationContainer>
         </SafeAreaProvider>
     )
-}
+})
